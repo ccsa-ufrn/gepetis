b6e5dc10f1c39eccec8a0ad22d0b29acf90f1aa7
Wil Holt <joswilder46@gmail.com>
2019-01-30 16:36:17 -0300

    update

--- src/app/dashboard/dash-components/edit-submissions/edit-submissions.component.ts
+++ src/app/dashboard/dash-components/edit-submissions/edit-submissions.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-submissions.component.css']
 })
 export class EditSubmissionsComponent implements OnInit {
+  Submissions: Array<any> = [];
 
   constructor() { }
 
   ngOnInit() {
+    for(let i = 0 ; i< 5 ; i++){
+      let aux = {
+        id : i,
+        Title : i,
+        Authors: '22/01/2019',
+        User:'Jose Wilder',
+        Data: '29/03/1997'
+      }
+    this.Submissions.push(aux);
+
+    }
+
   }
 
 }
